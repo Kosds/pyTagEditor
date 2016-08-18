b3585bb5506f81428d4a12c8591ea11c0523e48c
kosds <Kos-dan@mail.ru>
2016-08-18 20:52:16 +0300

    PC-2016.1.4 <daniil@kosachev Overwrite remote https://github.com/Kosds/pyTagEditor to local

--- lastFmData.py
+++
@@ -1,9 +0,0 @@
-﻿import pylast
-
-API_KEY='70578b40668e460c6282ee394e448586'
-API_SECRET='d105089ed009bbd740176b4e00ffd261'
-url='ws.audioscrobbler.com'
-username='kosds'
-password_hash=pylast.md5('12481632!')
-
-network = pylast.LastFMNetwork(API_KEY,API_SECRET,username, password_hash)
